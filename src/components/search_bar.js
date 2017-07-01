e351a93bbdbb9c7ae874be39e7191f68d1f8a0c8
Boris Stephane NKUINJEU <nkuinjeustephane@gmail.com>
2017-07-01 21:26:31 +0200

    Add video assets and Improve search youtube

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,14 +5,21 @@ export default class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    const term = event.target.value;
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.setState({term: event.target.value})}
+          onChange={this.onInputChange}
         />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,22 +1,57 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_YOUTUBE_KEY = 'AIzaSyCGATr18LcnwOZLDlsZR-7TtqkPFUJ8GEo';
 
-YTSearch({key: API_YOUTUBE_KEY, term: 'surfboard'}, function (data) {
-  console.log(data);
-});
-
 // Take a component and render some HTML content
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  )
-};
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('React JS');
+    this.onVideoSelect = this.onVideoSelect.bind(this);
+  }
+
+  // Todo Créer une promise qui fait le job
+  videoSearch(term) {
+    YTSearch({key: API_YOUTUBE_KEY, term: term}, videos => {
+      console.log(videos);
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  onVideoSelect(selectedVideo) {
+     this.setState({selectedVideo});
+  }
+
+  render() {
+    const videoSearchDebounce = _.debounce(term => this.videoSearch(term), 300);
+
+    // Todo Gérer les videos assets qui apparaîtront si pas de connexion
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearchDebounce}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
 
 // Take component and render it in the browser
 ReactDom.render(<App/>, document.querySelector('.container'));
